a84b5fd4bce64ada349dda91daa82100aed8a494
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2013-09-25 23:21:13 -0700

    Add Lesson Two.

--- LoopBackGuideApplication/LoopBackGuideApplication/LessonTwoView.m
+++ LoopBackGuideApplication/LoopBackGuideApplication/LessonTwoView.m
@@ -12,13 +12,23 @@
 
 #import "AppDelegate.h"
 
+/**
+ * Unlike Lesson One, our WeaponModel class is based _entirely_ on an existing schema.
+ *
+ * In this case, every field in Oracle that's defined as a NUMBER type becomes an NSNumber,
+ * and each field defined as a VARCHAR2 becomes an NSString.
+ *
+ * When we load these models from Oracle, LoopBack uses these @property declarations to know
+ * what data we care about. If we left off `extras`, for example, LoopBack would simply omit
+ * that field.
+ */
 @interface WeaponModel : LBModel
 
 @property (nonatomic, copy) NSString *id;
 @property (nonatomic, copy) NSString *name;
-@property (nonatomic) int audibleRange;
-@property (nonatomic) int effectiveRange;
-@property (nonatomic) int rounds;
+@property (nonatomic, copy) NSNumber *audibleRange;
+@property (nonatomic, copy) NSNumber *effectiveRange;
+@property (nonatomic, copy) NSNumber *rounds;
 @property (nonatomic, copy) NSString *extras;
 @property (nonatomic, copy) NSString *fireModes;
 
@@ -28,6 +38,9 @@
 
 @end
 
+/**
+ * Our custom ModelPrototype subclass. See Lesson One for more information.
+ */
 @interface WeaponModelPrototype : LBModelPrototype
 
 + (instancetype)prototype;
@@ -44,39 +57,57 @@
 
 @end
 
+/**
+ * Private Intervace for Lesson Two: Existing Data? No Problem.
+ */
 @interface LessonTwoView()
 
 @property (strong, nonatomic) NSArray *weapons;
 
 @end
 
+/**
+ * Implementation for Lesson Two: Existing Data? No Problem.
+ */
 @implementation LessonTwoView
 
-- (id)initWithFrame:(CGRect)frame
-{
-    self = [super initWithFrame:frame];
+- (id)init {
+    NSLog(@"init");
+    return [super init];
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
 
     if (self) {
         self.weapons = @[];
-        [self.resultsTable registerNib:[UINib nibWithNibName:@"LessonTwoTableCell" bundle:[NSBundle mainBundle]] forCellReuseIdentifier:@"Cell"];
+
+//        [self.resultsTable registerNib:[UINib nibWithNibName:@"LessonTwoTableCell" bundle:[NSBundle mainBundle]] forCellReuseIdentifier:@"Cell"];
     }
 
     return self;
 }
 
+/**
+ * Basic UITableViewDataSource implementation using our custom LBModel type.
+ */
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    NSLog(@"Getting rows: %lu", (unsigned long)self.weapons.count);
-
     return self.weapons.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    UITableViewCell *cell = [[[NSBundle mainBundle] loadNibNamed:@"LessonTwoTableCell" owner:self options:nil] objectAtIndex:0];
 
     WeaponModel *model = (WeaponModel *)self.weapons[indexPath.row];
 
-    NSLog(@"Trying to assign text label for %@", model.name);
     cell.textLabel.text = model.name;
+
+    if ([model.effectiveRange isKindOfClass:[NSNumber class]]) {
+        cell.detailTextLabel.text = [NSString stringWithFormat:@"%@m", model.effectiveRange];
+    } else {
+        cell.detailTextLabel.text = @"-";
+    }
 
     return cell;
 }
@@ -86,25 +117,36 @@
 }
 
 /**
- * Saves the desired Ammo model to the server with all values pulled from the UI.
+ * Loads all Weapon models from the server. To make full use of this, return to your (running) Sample Application
+ * and restart it with the DB environment variable set to "oracle". For example, on most *nix flavors (including
+ * Mac OS X), that looks like:
+ *
+ * 1. Stop the current server with Ctrl-C.
+ * 2. DB=oracle slc run app
+ *
+ * What does this do, you ask? Without that environment variable, the Sample Application uses simple, in-memory
+ * storage for all models. With the environment variable, it uses a custom-made Oracle adapter with a demo
+ * Oracle database we host for this purpose. If you have existing data, it's that easy to pull into LoopBack.
+ * No need to leave it behind.
+ *
+ * Advanced users: LoopBack supports multiple data sources simultaneously, albeit on a per-model basis. In your
+ * next project, try connecting a schema-less model (e.g. our Ammo example) to a Mongo data source, while
+ * connecting a legacy model (e.g. this Weapon example) to an Oracle data source.
  */
 - (IBAction)sendRequest:(id)sender {
     // 1. Grab the shared LBRESTAdapter instance.
     LBRESTAdapter *adapter = ((AppDelegate *)[[UIApplication sharedApplication] delegate]).adapter;
 
-    // 2. Instantiate our AmmoModelPrototype. For the intrepid, notice that we could create this
-    //    once (say, in initWithFrame:) and use the same instance for every request. Additionally,
-    //    the shared adapter is associated with the prototype, so we'd only have to do step 1 in
-    //    initWithFrame: also. This more verbose version is presented as an example; making it more
-    //    efficient is left as a rewarding exercise for the reader.
+    // 2. Instantiate our AmmoModelPrototype. See LessonOneView for further discussion.
     WeaponModelPrototype *prototype = (WeaponModelPrototype *)[adapter prototypeWithClass:[WeaponModelPrototype class]];
 
+    // 3. Rather than instantiate a model directly like we did in Lesson One, we'll query the server for
+    //    all Weapons, filling out our UITableView with the results. In this case, the Prototype is really
+    //    the workhorse; the Model is just a simple container.
     [prototype allWithSuccess:^(NSArray *models) {
-        NSLog(@"Models: %@", models);
-        
+        NSLog(@"Successfully loaded all Weapon models.");
         self.weapons = models;
-        NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
-        [self.resultsTable insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+        [self.resultsTable performSelectorOnMainThread:@selector(reloadData) withObject:nil waitUntilDone:NO];
     } failure:^(NSError *error) {
         NSLog(@"Failed to get all with %@", error);
         [[[UIAlertView alloc] initWithTitle:@"Error"
